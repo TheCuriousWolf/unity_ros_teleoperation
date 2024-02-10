67f85e55ef5b059732db03a1b3564566eb6b9933
Maximum Wilder-Smith <maxwildersmith@gmail.com>
2024-02-10 02:24:02 +0100

    added voxblox mesh viz

--- Assets/Components/VoxBlox/VoxbloxMesh.cs
+++ Assets/Components/VoxBlox/VoxbloxMesh.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Robotics.ROSTCPConnector;
 using RosMessageTypes.Voxblox;
+using System;
 
 public class VoxbloxMesh : MonoBehaviour
 {
     public string topic = "/voxblox_node/mesh";
+    public Material material;
     private ROSConnection _ros;
-    private Mesh mesh;
+    public bool debug = false;
+    public float size = 0.5f;
+    private Dictionary<Tuple<long, long, long>, GameObject> blocks;
+
+
     void Start()
     {
-        mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
+
+        blocks = new Dictionary<Tuple<long, long, long>, GameObject>();
 
         _ros = ROSConnection.GetOrCreateInstance();
         _ros.Subscribe<MeshMsg>(topic, UpdateMesh);
@@ -20,35 +26,62 @@ public class VoxbloxMesh : MonoBehaviour
 
     void UpdateMesh(MeshMsg tmpMesh)
     {
-        Debug.Log("Updating mesh: " + tmpMesh.mesh_blocks.Length + " blocks.");
+        // Debug.Log("Updating mesh: " + tmpMesh.mesh_blocks.Length + " blocks.");
 
-        MeshBlockMsg[] blocks = tmpMesh.mesh_blocks;
+        MeshBlockMsg[] meshBlocks = tmpMesh.mesh_blocks;
         float block_edge_length = tmpMesh.block_edge_length;
 
         float point_conv_factor = 2f/System.UInt16.MaxValue;
 
-        List<Vector3> vertices = new List<Vector3>();
-        List<int> triangles = new List<int>();
-        List<Color> colors = new List<Color>();
-        // add current mesh to the list
-        vertices.AddRange(mesh.vertices);
-        triangles.AddRange(mesh.triangles);
-        colors.AddRange(mesh.colors);
-
 
         bool hasColor = false;
+        string blocksStr = "";
 
-        for(int j=0; j<blocks.Length; j++)
+        Mesh mesh;
+
+        for(int j=0; j<meshBlocks.Length; j++)
         {
-            MeshBlockMsg block = blocks[j];
+            MeshBlockMsg block = meshBlocks[j];
             hasColor = block.r.Length == block.x.Length;
             Color color;
+            
+            List<Vector3> vertices = new List<Vector3>();
+            List<int> triangles = new List<int>();
+            List<Color> colors = new List<Color>();
+            int vertexCount = 0;
+
+            Tuple<long, long, long> index = new Tuple<long, long, long>(block.index[0], block.index[1], block.index[2]);
+
+            if(!blocks.ContainsKey(index))
+            {
+                GameObject blockObj = new GameObject("Block " + index.Item1 + " " + index.Item2 + " " + index.Item3);
+                blockObj.transform.parent = transform;
+                blockObj.transform.localPosition = Vector3.zero;
+                blockObj.transform.localRotation = Quaternion.identity;
+                blocks.Add(index, blockObj);
+
+                mesh = new Mesh();
+                blockObj.AddComponent<MeshFilter>().mesh = mesh;
+                blockObj.AddComponent<MeshRenderer>().material = material;
+            } else
+            {
+                mesh = blocks[index].GetComponent<MeshFilter>().mesh;
+            }
+
+            mesh.Clear();
+
+
             for(int i=0; i<block.x.Length; i++)
             {
                 float x = ((float)block.x[i] * point_conv_factor + (float)block.index[0]) * block_edge_length;
                 float y = ((float)block.y[i] * point_conv_factor + (float)block.index[1]) * block_edge_length;
                 float z = ((float)block.z[i] * point_conv_factor + (float)block.index[2]) * block_edge_length;
                 vertices.Add(new Vector3(x, y, z));
+
+                if(debug)
+                {
+                    Debug.DrawRay(new Vector3(x, y, z), Vector3.up*size, Color.red,20f);
+                }
 
                 if(hasColor)
                 {
@@ -59,17 +92,16 @@ public class VoxbloxMesh : MonoBehaviour
                 }
                 colors.Add(color);
 
-                triangles.Add(i);
+                triangles.Add(vertexCount);
+                vertexCount++;
             }
+            mesh.vertices = vertices.ToArray();
+            mesh.triangles = triangles.ToArray();
+            mesh.colors = colors.ToArray();
+            mesh.RecalculateNormals();
+            mesh.RecalculateBounds();
         }
 
-
-        mesh.Clear();
-        mesh.vertices = vertices.ToArray();
-        mesh.triangles = triangles.ToArray();
-        mesh.colors = colors.ToArray();
-        mesh.RecalculateNormals();
-        mesh.RecalculateBounds();
     }
 
 }
